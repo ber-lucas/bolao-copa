1643a6c475007917f20a171a5875a351665e1d2c
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2022-11-02 23:38:25 -0300

    context auth and signIn page created

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,28 @@
-import { Center, Text } from "native-base";
+import { Fontisto } from '@expo/vector-icons';
+import { Center, Icon, Text } from "native-base";
+
+import Logo from '../assets/logo.svg';
+import { Button } from "../components/Button";
+import { useAuth } from '../hooks/useAuth';
 
 export function SignIn() {
+  const { signIn } = useAuth()
+
   return (
-    <Center flex={1} bgColor='gray.900'>
-      <Text color={'white'} fontSize={24} fontFamily='heading'>
-        SignIn
+    <Center flex={1} bgColor='gray.900' p={7}>
+      <Logo width={212} height={40}/>
+
+      <Button 
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name='google' color={'white'} size='md' />}
+        type='SECONDARY'
+        mt={12}
+        onPress={signIn}
+      />
+
+      <Text color={'white'} textAlign='center' mt={4}>
+        Não utilizamos nenhuma informação além do seu e-mail para a criação
+        da sua conta.
       </Text>
     </Center>
   )
